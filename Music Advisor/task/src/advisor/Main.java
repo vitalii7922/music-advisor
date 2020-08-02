da41d0357dfe5128b3fcb445e745a88d981d4504
vitalii7922 <nefedovvi792@gmail.com>
2020-08-02 21:40:51 +0300

    completed second stage

--- Music Advisor/task/src/advisor/Main.java
+++ Music Advisor/task/src/advisor/Main.java
@@ -5,26 +5,49 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    private static boolean access = false;
+    private static final String URL = "https://accounts.spotify.com/authorize?" +
+            "client_id=c272e24c020d428f848594eea7f5199d&" +
+            "redirect_uri=http://localhost:8080&response_type=code";
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Advice advice = new Advice();
         while (true) {
             switch (reader.readLine()) {
                 case "new":
-                    System.out.println("---NEW RELEASES---");
-                    advice.getNewReleases().forEach(System.out::println);
+                    if (access) {
+                        System.out.println("---NEW RELEASES---");
+                        advice.getNewReleases().forEach(System.out::println);
+                    } else {
+                        printMessage();
+                    }
                     break;
                 case "featured":
-                    System.out.println("---FEATURED---");
-                    advice.getFeatured().forEach(System.out::println);
+                    if (access) {
+                        System.out.println("---FEATURED---");
+                        advice.getFeatured().forEach(System.out::println);
+                    } else {
+                        printMessage();
+                    }
                     break;
                 case "categories":
-                    System.out.println("---CATEGORIES---");
-                    advice.getCategories().forEach(System.out::println);
+                    if (access) {
+                        System.out.println("---CATEGORIES---");
+                        advice.getCategories().forEach(System.out::println);
+                    } else {
+                        printMessage();
+                    }
                     break;
                 case "playlists Mood":
-                    System.out.println("---MOOD PLAYLISTS---");
-                    advice.getCategories().forEach(System.out::println);
+                    if (access) {
+                        System.out.println("---MOOD PLAYLISTS---");
+                        advice.getCategories().forEach(System.out::println);
+                    } else {
+                        printMessage();
+                    }
+                    break;
+                case "auth":
+                    authentication();
                     break;
                 case "exit":
                     System.out.println("---GOODBYE!---");
@@ -33,5 +56,16 @@ public class Main {
                     break;
             }
         }
+    }
+
+    private static void printMessage() {
+        System.out.println("Please, provide access for application.");
+    }
+
+
+    private static void authentication() {
+            access = true;
+            System.out.println(URL);
+            System.out.println("---SUCCESS---");
     }
 }
