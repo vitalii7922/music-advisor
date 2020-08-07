ef9b22b8724e22161a9f2378b27d3d42ced24f6c
vitalii7922 <nefedovvi792@gmail.com>
2020-08-07 13:47:40 +0300

    refactored strategy

--- Music Advisor/task/src/advisor/Strategy/TurningMethods.java
+++ Music Advisor/task/src/advisor/Strategy/TurningMethods.java
@@ -1,6 +1,51 @@
 package advisor.Strategy;
 
-interface TurningMethods {
-    void turnPageForward();
-    void turnPageBackward();
+import java.util.List;
+
+abstract class TurningMethods {
+    private int elementsNumber;
+    private List<String> output;
+    private int currentPage;
+    private int pagesNumber;
+
+    public TurningMethods(int elementsNumber, List<String> output) {
+        this.elementsNumber = elementsNumber;
+        this.output = output;
+        if (elementsNumber != 0) {
+            pagesNumber = output.size() / elementsNumber;
+        }
+    }
+
+    public void turnPageForward() {
+        if (output.size() > elementsNumber) {
+            if ((currentPage + 1) * elementsNumber <= output.size()) {
+                currentPage++;
+                printPage();
+            } else {
+                System.out.println("No more pages");
+            }
+        } else {
+            output.forEach(System.out::println);
+        }
+    }
+
+    public void turnPageBackward() {
+        if (output.size() > elementsNumber) {
+            if (currentPage > 1) {
+                currentPage--;
+                printPage();
+            } else {
+                System.out.println("No more pages");
+            }
+        } else {
+            output.forEach(System.out::println);
+        }
+    }
+
+    private void printPage() {
+        for (int i = (currentPage - 1) * elementsNumber; i < elementsNumber * currentPage; i++) {
+            System.out.println(output.get(i));
+        }
+        System.out.println("---PAGE " + currentPage + " OF " + pagesNumber + "--");
+    }
 }
--- Music Advisor/task/src/advisor/Strategy/TurningPagesCategories.java
+++ Music Advisor/task/src/advisor/Strategy/TurningPagesCategories.java
@@ -2,21 +2,22 @@ package advisor.Strategy;
 
 import java.util.List;
 
-public class TurningPagesCategories implements TurningMethods {
-    private int elementsNumber;
+public class TurningPagesCategories extends TurningMethods {
+/*    private int elementsNumber;
     private List<String> output;
     private int currentPage;
-    private int pagesNumber;
+    private int pagesNumber;*/
 
     public TurningPagesCategories(int elementsNumber, List<String> output) {
-        this.elementsNumber = elementsNumber;
-        this.output = output;
-        if (elementsNumber != 0) {
+        super(elementsNumber, output);
+//        this.elementsNumber = elementsNumber;
+//        this.output = output;
+        /*if (elementsNumber != 0) {
             pagesNumber = output.size() / elementsNumber;
-        }
+        }*/
     }
 
-    @Override
+    /*@Override
     public void turnPageForward() {
         if (output.size() > elementsNumber) {
             if ((currentPage + 1) * elementsNumber <= output.size()) {
@@ -48,5 +49,5 @@ public class TurningPagesCategories implements TurningMethods {
         } else {
             output.forEach(System.out::println);
         }
-    }
+    }*/
 }
