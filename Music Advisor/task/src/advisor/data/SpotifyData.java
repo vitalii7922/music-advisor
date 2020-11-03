d303f959373f56494b03cab5c2b6f1af1554619f
vitalii7922 <nefedovvi792@gmail.com>
2020-11-03 23:51:50 +0300

    add spotify data

---
+++ Music Advisor/task/src/advisor/data/SpotifyData.java
@@ -0,0 +1,14 @@
+package advisor.data;
+
+public class SpotifyData {
+    private static final String CLIENT_ID = "aaaaaa9999123124njndskjg324";
+    private static final String CLIENT_SECRET = "asdausih324235njknk34543";
+
+    public static String getClientId() {
+        return CLIENT_ID;
+    }
+
+    public static String getClientSecret() {
+        return CLIENT_SECRET;
+    }
+}
