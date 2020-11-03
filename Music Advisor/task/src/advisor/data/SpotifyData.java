0a64ebf6049f0505e1fa7350d5d83434d29cb3f9
vitalii7922 <nefedovvi792@gmail.com>
2020-11-03 23:50:15 +0300

    change description

--- Music Advisor/task/src/advisor/data/SpotifyData.java
+++ Music Advisor/task/src/advisor/data/SpotifyData.java
@@ -2,7 +2,7 @@ package advisor.data;
 
 public class SpotifyData {
     private static final String CLIENT_ID = "c272e24c020d428f848594eea7f5199d";
-    private static final String CLIENT_SECRET = "583af1b361fb47598bd14c9f1fdf386c";
+    private static final String CLIENT_SECRET = "sdaaksdaq2131426toasdasdasdw";
 
     public static String getClientId() {
         return CLIENT_ID;
